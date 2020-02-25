e425785db37bbaa696d986318c176f9d2307876f
Jc Vial <jean-charles_vial@hotmail.fr>
2020-02-25 15:47:04 +0100

    init-cloudinary

--- Gemfile
+++ Gemfile
@@ -30,3 +30,4 @@ group :development, :test do
 end
 
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.12.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,18 +14,15 @@ user3 = User.create!(email: "marco@test.fr", first_name: "Marco", last_name: "Te
 user4 = User.create!(email: "julio@test.fr", first_name: "Julio", last_name: "Test", password: "123456", password_confirmation: "123456")
 
 
-
 vespa1 = Vespa.create!(name: "Magnifique Vespa Primavera 1978 pour découvrir la belle ville de Saint-Etienne", model: "Vespa Primavera 1978", cylinder: "50cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 39, address: "1 Place Villebœuf, 42100 Saint-Étienne", user_id: user1.id)
 vespa2 = Vespa.create!(name: "Magnifique Vespa Nuova 1965 pour découvrir la belle ville de Saint-Etienne", model: "Vespa Nuova 1965", cylinder: "125cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 49, address: "1 Place Villebœuf, 42100 Saint-Étienne", user_id: user1.id)
 vespa3 = Vespa.create!(name: "Magnifique Vespa GT 1923 découvrir la belle ville de Saint-Etienne", model: "Vespa GT 1923", cylinder: "250cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 99, address: "1 Place Villebœuf, 42100 Saint-Étienne", user_id: user1.id)
 vespa4 = Vespa.create!(name: "Magnifique Vespa sprint 1754 pour découvrir la belle ville de Saint-Etienne", model: "Vespa sprint 1754", cylinder: "300cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 319, address: "1 Place Villebœuf, 42100 Saint-Étienne", user_id: user1.id)
 vespa5 = Vespa.create!(name: "Magnifique Vespa Primavera Turbo Diesel Sport 2019 pour découvrir la belle ville de Saint-Etienne", model: "Vespa Primavera Turbo Diesel Sport 2019", cylinder: "electrique", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 999, address: "1 Place Villebœuf, 42100 Saint-Étienne", user_id: user1.id)
-
 vespa6 = Vespa.create!(name: "Escapade en amoureux dans le forez sur notre Vespa Primavera 1978", model: "Vespa Primavera 1978", cylinder: "50cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 35, address: "16 Rue Georges Teissier, 42000 Saint-Étienne", user_id: user2.id)
 vespa7 = Vespa.create!(name: "Escapade en amoureux dans le forez sur notre Vespa Nuova 1965", model: "Vespa Primavera 1978", cylinder: "125cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 54, address: "16 Rue Georges Teissier, 42000 Saint-Étienne", user_id: user2.id)
 vespa8 = Vespa.create!(name: "Escapade en amoureux dans le forez sur notre Vespa GT 1923", model: "Vespa GT 1923", cylinder: "250cc", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 18, address: "16 Rue Georges Teissier, 42000 Saint-Étienne", user_id: user2.id)
 vespa9 = Vespa.create!(name: "Escapade en amoureux dans le forez sur notre Vespa sprint 1754", model: "Vespa sprint 1754", cylinder: "electrique", description: "Rouler avec style et faites de la Primavera votre première Vespa. Le moteur 4 temps 50cc refroidi par air, équipé d'une injection électronique, vous assure qualité et fiabilité. Il permet une réduction extraordinaire de la consommation de carburant et des émissions de CO2. Le moteur parfait pour votre premier véhicule. Il ne vous reste plus qu'à choisir la couleur qui vous correspond le mieux.", price: 24, address: "16 Rue Georges Teissier, 42000 Saint-Étienne", user_id: user2.id)
-
 
 
 booking1 = Booking.create!(date: "Sat, 11 Nov 2007", confirmation: false, user_id: user3.id, vespa_id: vespa1.id)
@@ -43,3 +40,26 @@ review2 = Review.create!(coment: "Quelle ville immonde !", rating: 1, booking_id
 review3 = Review.create!(coment: "Sympa malgré les trompes d'eau ! Heureusement la Vespa est équipée d'une belle jupe !", rating: 5, booking_id: booking3.id)
 review4 = Review.create!(coment: "Bof !", rating: 3, booking_id: booking5.id)
 review5 = Review.create!(coment: "A refaire, je recommande !", rating: 4, booking_id: booking1.id)
+
+
+
+require "open-uri"
+
+file1 = URI.open('https://images.pexels.com/photos/3599586/pexels-photo-3599586.jpeg?cs=srgb&dl=orange-motorcycle-on-gray-concrete-road-3599586.jpg&fm=jpg')
+vespa1.photo.attach(io: file1, filename: 'vespa1.png', content_type: 'image/png')
+file2 = URI.open('https://images.pexels.com/photos/221299/pexels-photo-221299.jpeg?cs=srgb&dl=architecture-automotive-building-cafe-221299.jpg&fm=jpg')
+vespa2.photo.attach(io: file2, filename: 'vespa2.png', content_type: 'image/png')
+file3 = URI.open('https://images.pexels.com/photos/995487/pexels-photo-995487.jpeg?cs=srgb&dl=photography-of-classic-motorcycle-on-road-995487.jpg&fm=jpg')
+vespa3.photo.attach(io: file3, filename: 'vespa3.png', content_type: 'image/png')
+file4 = URI.open('https://www.artisanat.ch/images/reportages/routes/carrosse-maxi.jpg')
+vespa4.photo.attach(io: file4, filename: 'vespa4.png', content_type: 'image/png')
+file5 = URI.open('https://images.pexels.com/photos/2983665/pexels-photo-2983665.jpeg?cs=srgb&dl=woman-pushing-a-scooter-2983665.jpg&fm=jpg')
+vespa5.photo.attach(io: file5, filename: 'vespa5.png', content_type: 'image/png')
+file6 = URI.open('https://images.pexels.com/photos/1173240/pexels-photo-1173240.jpeg?cs=srgb&dl=photo-of-man-leaning-on-motorcycle-1173240.jpg&fm=jpg')
+vespa6.photo.attach(io: file6, filename: 'vespa6.png', content_type: 'image/png')
+file7 = URI.open('https://images.pexels.com/photos/2406775/pexels-photo-2406775.jpeg?cs=srgb&dl=photo-of-pink-motor-scooter-parked-next-to-pink-wall-2406775.jpg&fm=jpg')
+vespa7.photo.attach(io: file7, filename: 'vespa7.png', content_type: 'image/png')
+file8 = URI.open('https://images.pexels.com/photos/1662556/pexels-photo-1662556.jpeg?cs=srgb&dl=parked-blue-motor-scooter-1662556.jpg&fm=jpg')
+vespa8.photo.attach(io: file8, filename: 'vespa8.png', content_type: 'image/png')
+file9 = URI.open('https://images.pexels.com/photos/1368554/pexels-photo-1368554.jpeg?cs=srgb&dl=woman-sitting-on-motor-scooter-1368554.jpg&fm=jpg')
+vespa9.photo.attach(io: file9, filename: 'vespa9.png', content_type: 'image/png')
