73bf4b15cc63b0cdce96639528fb48cd22e8c4c9
VictorCt <victor.c.boncoin@gmail.com>
2020-02-25 12:48:36 +0100

    add vespas controller & create new offer

---
+++ app/controllers/vespas_controller.rb
@@ -0,0 +1,35 @@
+class VespasController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :show]
+
+  def index
+    @vespas = Vespa.all
+  end
+
+  def new
+    @vespa = Vespa.new
+  end
+
+  def create
+    @vespa = Vespa.new(vespas_strong_params)
+    @vespa.user = current_user
+    if @vespa.save
+      redirect_to vespa_path(@vespa)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @vespa = Vespa.find(params[:id])
+  end
+
+  def destroy
+  end
+
+  private
+
+   def vespas_strong_params
+    params.require(:vespa).permit(:name, :model, :cylinder, :description, :price, :address )
+  end
+
+end
